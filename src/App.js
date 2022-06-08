d5d9f37e0a7bd166c88b58b49a3be6cb5e65702c
KonstantinSav <koston022@mail.ru>
2022-06-08 16:56:54 +0500

    add get local storage

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ import './App.css';
 import HeaderApp from './components/headerApp/headerApp';
 import BookForm from './components/bookForm/BookForm';
 import BookList from './components/bookList/BookList';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {useEffect} from 'react'
+import {setBooks} from './app/bookSlice'
 function App() {
     const books = useSelector((state) => state.books.books);
+    const dispatch = useDispatch()
+    
+    useEffect(()=>{
+        console.log('get localstorage.....');
+        const getLocalstorage = JSON.parse(localStorage.getItem('books'));
+        console.log('getLocalstorage', getLocalstorage);
+        if (getLocalstorage.length) {
+            console.log('set dispatch....');
+            dispatch(setBooks(getLocalstorage))
+            
+        }
+    }, [])
+
     useEffect(() => {
         localStorage.setItem('books', JSON.stringify(books))
         
