c7cb6df48b40cc7bcfa56c1cc2d6151c0f2f9b9b
KonstantinSav <koston022@mail.ru>
2022-06-08 14:19:50 +0500

    set localstorage

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 import HeaderApp from './components/headerApp/headerApp';
 import BookForm from './components/bookForm/BookForm';
 import BookList from './components/bookList/BookList';
+import {useSelector} from 'react-redux';
+import {useEffect} from 'react'
 function App() {
+    const books = useSelector((state) => state.books.books);
+    useEffect(() => {
+        localStorage.setItem('books', JSON.stringify(books))
+        
+    }, [books]);
     return (
         <div className='app'>
             <HeaderApp />
